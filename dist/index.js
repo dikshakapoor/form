92c8809f810ea955dc1774d0343359e63d065568
Diksha Kapoor <dkapoor@tekion.com>
2021-02-05 20:16:38 +0530

    first commit

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-// import {makeRows} from './view';
-import GameController from './controller/gameController';
-
-const gameController = new GameController();
-gameController.play();
-document.addEventListener("keyup", gameController.keyPressEvent)
-document.addEventListener("click",gameController.onClickEvent )
--- index.js
+++
@@ -1,6 +0,0 @@
-import GameController from './controller/gameController';
-
-const gameController = new GameController();
-gameController.play();
-document.addEventListener("keyup", gameController.keyPressEvent)
-document.addEventListener("click",gameController.onClickEvent )
